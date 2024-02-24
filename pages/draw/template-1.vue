3d69477e036b95b77f058891ea858e369aeba713
Ahmad Ghanavati <Ahmad_ghanavati@yahoo.com>
2024-02-24 17:50:23 +0330

    add Drawing Tools

---
+++ pages/draw/template-1.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <Draw/>
+  </div>
+</template>
+
+<style scoped>
+#map {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="flex flex-col space-y-3 p-10">
+  <nuxt-link to="/map" class="text-blue-500 font-bold"> - Simple Map </nuxt-link>
+  <nuxt-link to="/draw/template-1" class="text-blue-500 font-bold"> - Draw ( Template 1 ) </nuxt-link>
+</div>
+</template>
+
+<style scoped>
+
+</style>
